18038c2a8a08edeb30b36bf7836ace36ef629d57
Dan Jones <danjones@goodevilgenius.org>
2021-07-19 10:50:43 -0500

    💬 Change LDS lingo in "Can't Do Math"

--- .dir-locals.el
+++ .dir-locals.el
@@ -3,7 +3,7 @@
                    (time-stamp-start . "^modified:[ \t]+[\"]?")
                    (time-stamp-end . "[\"]?$")
                    (time-stamp-line-limit . 15)
-                   (time-stamp-format . "%Y-%02m-%02d %02H:%02M:%02S")
+                   (time-stamp-format . "%Y-%02m-%02dT%02H:%02M:%02S%5z")
                    ))
 
  )
